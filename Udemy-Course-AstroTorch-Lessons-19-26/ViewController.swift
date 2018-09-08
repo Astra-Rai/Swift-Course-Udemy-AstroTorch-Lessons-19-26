b7029c2d8b568667c775230c5da34a3b108ad3ee
Astra Rai <astrarai.com@gmail.com>
2018-09-07 21:19:45 -0700

    -code added to toggle background color -background color white when view is loaded (issue to be corrected )

--- Udemy-Course-AstroTorch-Lessons-19-26/ViewController.swift
+++ Udemy-Course-AstroTorch-Lessons-19-26/ViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var screenIsRed = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 }
     @IBAction func buttonClicked(_ sender: Any) {
-    
-    self.view.backgroundColor = UIColor.red;
+        if(screenIsRed){
+          self.view.backgroundColor = UIColor.blue
+          screenIsRed = false
+        }else{
+        self.view.backgroundColor = UIColor.red
+        screenIsRed = true
+        }
     }
 }
-
