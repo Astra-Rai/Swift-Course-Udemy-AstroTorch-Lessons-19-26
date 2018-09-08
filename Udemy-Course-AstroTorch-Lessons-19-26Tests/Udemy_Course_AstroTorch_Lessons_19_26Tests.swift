4a7d44a2857baf5021934488bf4f5adf1aeb388b
Astra Rai <astrarai.com@gmail.com>
2018-09-07 19:46:10 -0700

    Initial Commit

---
+++ Udemy-Course-AstroTorch-Lessons-19-26Tests/Udemy_Course_AstroTorch_Lessons_19_26Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Udemy_Course_AstroTorch_Lessons_19_26Tests.swift
+//  Udemy-Course-AstroTorch-Lessons-19-26Tests
+//
+//  Created by astra daniels on 9/7/18.
+//  Copyright © 2018 astra daniels. All rights reserved.
+//
+
+import XCTest
+@testable import Udemy_Course_AstroTorch_Lessons_19_26
+
+class Udemy_Course_AstroTorch_Lessons_19_26Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
